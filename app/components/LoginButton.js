3c3d30c1fcd1487e4b8c431618941cc46d059a85
jeppan875 <jeppan875@gmail.com>
2019-04-13 13:09:34 +0200

    styled auth buttons

--- app/components/LoginButton.js
+++ app/components/LoginButton.js
@@ -1,10 +1,24 @@
 import { GIT_AUTH_LOGIN } from '../api'
+import styled from 'styled-components'
+
+export const AuthButton = styled.button`
+color: white;
+background-color: black;
+font-size: 1em;
+margin: 1em;
+padding: 0.25em 1em;
+border: 2px solid white;
+border-radius: 3px;
+float: right;
+cursor: pointer;
+`;
 
 export default function LoginButton(props) {
     const { children, ...rest } = props;
+
     return (
         <div >
-            <a href={GIT_AUTH_LOGIN} >Login</a>
-        </div>
+            <a href={GIT_AUTH_LOGIN} ><AuthButton>Login</AuthButton></a>
+        </div >
     )
 }
--- app/components/LogoutButton.js
+++ app/components/LogoutButton.js
@@ -1,9 +1,11 @@
+import { AuthButton } from './LoginButton'
+
 export default function LogutButton(props) {
     const { children, logout, ...rest } = props;
 
     return (
         <div >
-            <button onClick={logout} >Logout</button>
+            <AuthButton onClick={logout} >Logout</AuthButton>
         </div>
     )
 }
