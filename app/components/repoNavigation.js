5cd4d2c589667efa42f4b184aa72b7d879c51c0c
jeppan875 <jeppan875@gmail.com>
2019-04-18 23:36:50 +0200

    fixed files

--- app/components/repoNavigation.js
+++ app/components/repoNavigation.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import Link from 'next/link'
 
 export const ListDiv = styled.div`
 background-color: #e6e9ef;
@@ -16,25 +15,8 @@ font-size: 20px;
 list-style-type: none;
 `;
 
-const StyledLink = styled.a`
+export const StyledLink = styled.a`
   color: black;
   font-weight: bold;
   text-decoration: none;
 `;
-
-function repoNavigator(props) {
-    const { repoLinks, name, fetchSingleRepo } = props;
-
-    return (
-        <ListDiv>
-            <UlStyle>
-                {repoLinks.map((item, index) =>
-                    <Link key={index} href={`repo?name=${name}&path=${item.path}`} passHref>
-                        <StyledLink><li onClick={() => fetchSingleRepo(name, item.path)} key={index}>{item.name}</li></StyledLink>
-                    </Link>)}
-            </UlStyle>
-        </ListDiv>
-    )
-}
-
-export default repoNavigator
--- app/containers/Repos/Repo/index.js
+++ app/containers/Repos/Repo/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { FETCH_REPO_CONTENT_SAGA } from '../constants'
-import RepoNavigation from 'components/repoNavigation'
+import { ListDiv, UlStyle, StyledLink } from 'components/repoNavigation'
 import { getRepoLinks } from '../selector'
+import Link from 'next/link'
+import { isArray } from 'util';
+
 const mapDispatchToProps = (dispatch) => {
     return {
-        fetchSingleRepo: (name, path) => dispatch({ type: FETCH_REPO_CONTENT_SAGA, name, path }),
+        fetchContent: (name, path) => dispatch({ type: FETCH_REPO_CONTENT_SAGA, name, path }),
     }
 }
 const mapStateToProps = (state, ownProps) => {
@@ -17,17 +20,28 @@ const mapStateToProps = (state, ownProps) => {
 class Repo extends Component {
 
     componentDidMount() {
-        const { name, fetchSingleRepo } = this.props
-        fetchSingleRepo(name, '/')
+        const { name, fetchContent } = this.props
+        fetchContent(name, '/')
     }
     render() {
-        const { name, repoLinks, path, fetchSingleRepo } = this.props
+        const { name, repoLinks, path, fetchContent } = this.props
+        const isDir = isArray(repoLinks)
+        const isFile = repoLinks && repoLinks.type === 'file'
         console.log(name)
         console.log(repoLinks)
         console.log(path)
+        console.log(isDir)
         return (
             <div>
-                {repoLinks && <RepoNavigation repoLinks={repoLinks} name={name} fetchSingleRepo={fetchSingleRepo} />}
+                {isDir && <ListDiv>
+                    <UlStyle>
+                        {repoLinks.map((item, index) =>
+                            <Link key={index} href={`repo?name=${name}&path=${item.path}`} passHref>
+                                <StyledLink><li onClick={() => fetchContent(name, item.path)} key={index}>{item.name}</li></StyledLink>
+                            </Link>)}
+                    </UlStyle>
+                </ListDiv>}
+                {isFile && <p>{atob(repoLinks.content)}</p>}
             </div>
         )
     }
